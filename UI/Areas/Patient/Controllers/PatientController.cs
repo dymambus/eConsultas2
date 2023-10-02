a24d20f791b4bbc44d9409dcca6f4a97f33a1cc9
Mysrath <mysrath@gmail.com>
2023-10-02 15:41:09 +0100

    Update PatientController.cs

--- UI/Areas/Patient/Controllers/PatientController.cs
+++ UI/Areas/Patient/Controllers/PatientController.cs
@@ -1,25 +1,99 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using LibBiz.Data;
+using LibBiz.Models;
+using Newtonsoft.Json;
+using UI.Models;
 
 namespace UI.Areas.Patient.Controllers
 {
     public class PatientController : Controller
     {
         private ILogger<PatientController> _logger;
+        private IBusinessMethods _BM;
 
-        public PatientController(ILogger<PatientController> logger)
+        public PatientController(ILogger<PatientController> logger, IBusinessMethods BM)
         {
             _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
+            _BM = BM;
         }
 
         [HttpGet]
-        public IActionResult PatientDashboard()
+        private LibBiz.Models.Patient? GetPatient()
         {
-            return View();
+            if (HttpContext.Session.GetString("Token") == null)
+            {
+                RedirectToAction("Login", "Auth");
+                return null;
+            }
+
+            var patient = _BM.GetPatientByEmail(HttpContext.Session.GetString("Email"));
+
+            if (patient != null)
+            {
+                var user = new LibBiz.Models.Patient()
+                {
+                    Name = patient.Name,
+                    Email = patient.Email,
+                    Phone = patient.Phone
+                };
+
+                return user;
+            }
+
+            RedirectToAction("Error", "Shared");
+
+            return null;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _logger.LogCritical("Chegou ao Index do paciente!"); // Demonstração do Logger
+
+            PatientAreaModel patient = new()
+            {
+                Patient = GetPatient()
+            };
+
+            return View(patient);
+        }
+
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            PatientAreaModel patient = new()
+            {
+                Patient = GetPatient()
+            };
+
+
+            return View(patient);
+        }
+
+        [HttpGet]
+        public IActionResult Search(PatientAreaModel? model = null)
+        {
+            if (model.Doctors == null)
+            {
+                model = new PatientAreaModel()
+                {
+                    Patient = GetPatient()
+                };
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Search()
+        {
+            PatientAreaModel searchModel = new()
+            {
+                Doctors = _BM.GetAllDoctors(),
+                Patient = GetPatient()
+            };
+
+            return View(searchModel);
         }
     }
 }
