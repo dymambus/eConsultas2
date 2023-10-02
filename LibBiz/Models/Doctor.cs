0bbb8e1ac0804150dc5931278c46e83c2e29d305
Mysrath <mysrath@gmail.com>
2023-10-02 15:43:31 +0100

    Update Doctor.cs

--- LibBiz/Models/Doctor.cs
+++ LibBiz/Models/Doctor.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +13,9 @@ namespace LibBiz.Models
         public string? City { get; set; }
         public string? Address { get; set; }
         public string? SpecializationName { get; set; }
+        public string? SpecializationDescription { get; set; }
         public int? Price { get; set; }
+        public string? PriceDescription { get; set; }
         public Photograph? Photograph { get; set; }
     }
 }
